d074cace056f0ba8fec46c3ccb25a3b3718e27a1
Leonardo Ferreyra <leferreyra@gmail.com>
2014-08-09 14:05:49 -0300

    Agrega sqlite a gitignore, agrega vista home, templates y archivos estaticos

--- hackatorce/urls.py
+++ hackatorce/urls.py
@@ -5,7 +5,9 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 
+	url(r'^$', 'website.views.Home'),
+
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
-	)
+)
