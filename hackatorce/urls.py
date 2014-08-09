59c13fdef6a9e443d0ac760800be4e58ee13e4de
francom77 <franco_m77@hotmail.com>
2014-08-09 14:30:56 -0300

    creadas vistas y urls

--- hackatorce/urls.py
+++ hackatorce/urls.py
@@ -5,7 +5,13 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 
+	#administrador
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
+    #API urls
+    #url(r'^api/actividades/airelibre$', 'website.views.APIActividadesLibre'),
+    #url(r'^api/actividades/adentro$', 'website.views.APIActividadesAdentro'),
+    url(r'^api/actividades$', 'website.views.APICrearActividad'),
+    url(r'^api/actividades/(?P<id_actividad>\d+)/unirse$', 'website.views.APIUnirse')
 	)
--- website/views.py
+++ website/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from website.models import Actividad, Participante
+from django.http import HttpResponse
+from django.core import serializers
+from json import dumps
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render_to_response, get_object_or_404
 
-# Create your views here.
+@csrf_exempt
+def APICrearActividad(request):
+
+	#creamos la actividad
+    actividad = Actividad(nombre = request.POST['nombre'],
+    	horarioInicio = request.POST['horarioInicio'],
+    	horarioFin = request.POST['horarioFin'],
+    	latitud = request.POST['latitud'],
+    	longitud = request.POST['longitud'],
+    	tipo = request.POST['tipo'])
+    
+    #la guardamos
+    actividad.save()
+
+    # devolvemos el status del la consulta
+    response = {
+        'status':'OK'
+    }
+    
+    return HttpResponse(dumps(response), mimetype='application/json')
+
+@csrf_exempt
+def APIUnirse(request, id_actividad):
+
+    actividad = get_object_or_404(Actividad, pk=id_actividad)
+    participante = Participante(nombre = request.POST['nombre'],
+        edad = request.POST['edad'])
+    actividad.participante.add(participante)
+
+    return HttpResponse(dumps(actividad), mimetype='application/json')
